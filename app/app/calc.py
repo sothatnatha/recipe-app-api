936af61ed0f6702bf993c16b80541d09a162b305
ThaSothatna <95077429+sothatnatha@users.noreply.github.com>
2023-06-03 15:13:58 +0700

    Add database service

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,11 +2,9 @@
 Calculator fucntion
 """
 
-
 def add(x, y):
     """ Add x and y then return result """
     return x + y
-
 
 def substract(x, y):
     """ Subtract x and y then return result """
