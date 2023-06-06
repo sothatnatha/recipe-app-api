1bc0eb65648f1868623011fd40c652b945990dbc
ThaSothatna <95077429+sothatnatha@users.noreply.github.com>
2023-06-06 18:42:46 +0700

    Support modifying users

--- app/core/tests/test_admins.py
+++ app/core/tests/test_admins.py
@@ -29,3 +29,13 @@ class AdminSiteTest(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test edit user page workks."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+
+
