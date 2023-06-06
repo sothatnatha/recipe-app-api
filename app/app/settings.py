a72bab9dffe6d8486bacde3546aba3bc04210c26
ThaSothatna <95077429+sothatnatha@users.noreply.github.com>
2023-06-06 21:27:22 +0700

    Configure api URLs & Test Swagger UI

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
     'rest_framework',
-    'drf-spectacular'
+    'core',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -134,5 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf-spectacular.openapi.AutoSchema',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
