37b4873a68c9813d7958d34af8e25b07d77fc627
ThaSothatna <95077429+sothatnatha@users.noreply.github.com>
2023-06-07 20:49:23 +0700

    Create Test and Implement User API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -32,7 +32,7 @@ class PublicUserApiTest(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        user = get_user_model().objects.create_user(email=payload['email'])
+        user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
