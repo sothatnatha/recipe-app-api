d5a3dad70bf7513c6e97cf267a87b97979731dfb
ThaSothatna <95077429+sothatnatha@users.noreply.github.com>
2023-06-08 18:05:07 +0700

    Implement manage user API

--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,7 @@
 View for the user api
 """
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -16,3 +16,13 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for the user."""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user view."""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieves and returns the authenticated user."""
+        return self.request.user
