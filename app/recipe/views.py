2184076b7d1425379f5e62fb6c21f529f8b25fd6
Thatna <95077429+thatna2211@users.noreply.github.com>
2023-06-20 14:33:13 +0700

    Implement uploading images to recipe APIs.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,5 +1,11 @@
 """View for the list of recipie apis."""
-from rest_framework import viewsets, mixins
+from rest_framework import(
+    viewsets,
+    mixins,
+    status
+)
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -24,12 +30,33 @@ class RecipeViewset(viewsets.ModelViewSet):
 
         if self.action == 'list':
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
     
     def perform_create(self, serializers):
         """Create a new recipe object."""
         serializers.save(user=self.request.user)
+
+    # Custom actions
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+        
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 
 class BaseRecipeAttrViewSet(mixins.ListModelMixin, 
