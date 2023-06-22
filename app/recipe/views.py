0d1391e73f02f9ba808e78f5bcfbf3555c457441
Thatna <95077429+thatna2211@users.noreply.github.com>
2023-06-22 20:45:54 +0700

    Implement tag and ingredient filtering

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -58,7 +58,6 @@ class RecipeViewset(viewsets.ModelViewSet):
         return queryset.filter(
             user=self.request.user
         ).order_by('-id').distinct()
-    
 
 
     def get_serializer_class(self):
@@ -93,8 +92,15 @@ class RecipeViewset(viewsets.ModelViewSet):
             )
         
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
 
-
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+           OpenApiParameter('assigned_only', OpenApiTypes.INT, enum=[0, 1]),
+        ]
+    )
+)
 class BaseRecipeAttrViewSet(mixins.ListModelMixin, 
                                 mixins.UpdateModelMixin,
                                 mixins.DestroyModelMixin, 
@@ -106,9 +112,16 @@ class BaseRecipeAttrViewSet(mixins.ListModelMixin,
 
     def get_queryset(self):
         """Filter queryset for the authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
 
-
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
 class TagViewset(BaseRecipeAttrViewSet):
     """View manage tags in the database."""
