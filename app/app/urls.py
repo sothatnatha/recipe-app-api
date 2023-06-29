8c6342ba2dec4659462163aefaf8676121a82ec5
Thatna <95077429+thatna2211@users.noreply.github.com>
2023-06-29 15:29:34 +0700

    add health check for api.

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from core import views as core_views
+
 from django.urls import path, include
 from drf_spectacular.views import(
     SpectacularAPIView,
@@ -25,6 +27,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/health-check/', core_views.health_check, name='health-check'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'),
          name='api-docs'),
     path('api/user/', include('user.urls')),
